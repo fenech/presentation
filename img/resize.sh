4a74e5a80e3e7de9f88b311559d35403688846e5
Tom Fenech <reggaebristol@gmail.com>
2013-03-26 12:25:09 +0000

    ready for talk

--- img/resize.sh
+++ img/resize.sh
@@ -4,9 +4,11 @@ for i in rad*; do
 	cd $i
 	for j in s*; do
 		cd $j
-		for f in *.png; do
-			cp $f _$f
-			convert $f -resize 50% $f
+		for f in [1-3].png; do
+			cp $f s_$f
+		done
+		for f in _[1-3].png; do
+			mv $f `echo $f | sed -e 's/^_//'` 
 		done
 		cd ..
 	done
